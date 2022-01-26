742b74a4c6925fa3caa1f120329ddc47315a800d
Zachary Sharkey <yekrahs@gmail.com>
2022-01-26 11:25:36 -0500

    Change to services

--- CommunityPlugin/Non Native Modifications/DisableServices.cs
+++ CommunityPlugin/Non Native Modifications/DisableServices.cs
@@ -1,4 +1,6 @@
 ﻿using CommunityPlugin.Objects;
+using CommunityPlugin.Objects.Args;
+using CommunityPlugin.Objects.Helpers;
 using CommunityPlugin.Objects.Interface;
 using EllieMae.EMLite.ePass.Services;
 using EllieMae.EMLite.UI;
@@ -11,9 +13,13 @@ using System.Windows.Forms;
 
 namespace CommunityPlugin.Non_Native_Modifications
 {
-    public class DisableServices:Plugin, ILoanTabChanged
+    public class DisableServices : Plugin, ILoanTabChanged, ILoanOpened, IFieldChange, INativeFormLoaded
     {
         private string ServicesToDisable;
+        private bool ShouldRun;
+        private const string Field = "CX.DISABLE.SERVICES";
+        private Form OpenForm;
+        private TabControl TabControl => (TabControl)OpenForm.Controls.Find("orderTab", true)[0];
 
         public override void LoanTabChanged(object sender, EventArgs e)
         {
@@ -26,7 +32,13 @@ namespace CommunityPlugin.Non_Native_Modifications
             t.Tick += T_Tick;
             t.Enabled = true;
         }
+        public override void LoanOpened(object sender, EventArgs e) { }
 
+        public override void FieldChanged(object sender, FieldChangeEventArgs e)
+        {
+            if (e.FieldID.Equals(Field))
+                Remove();
+        }
         private void T_Tick(object sender, EventArgs e)
         {
             Timer t = sender as Timer;
@@ -71,5 +83,58 @@ namespace CommunityPlugin.Non_Native_Modifications
             serviceItem.Visible = string.IsNullOrEmpty(ServicesToDisable);
         }
 
+        public override void NativeFormLoaded(object sender, FormOpenedArgs e)
+        {
+            if (e.OpenForm.Name.Equals("OrderDialog"))
+            {
+                OpenForm = e.OpenForm;
+                Timer t = new Timer();
+                t.Interval = 300;
+                t.Tick += T_Tick1;
+                t.Enabled = true;
+            }
+        }
+
+        private void T_Tick1(object sender, EventArgs e)
+        {
+            Timer t = sender as Timer;
+            t.Enabled = false;
+            ServicesToDisable = EncompassHelper.Val("CX.DISABLE.SERVICES");
+            RefreshGrid();
+            RefreshGrid();
+            TabControl.SelectedIndexChanged += TabControl_SelectedIndexChanged;
+        }
+
+        private void TabControl_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            RefreshGrid();
+            RefreshGrid(true);
+        }
+
+        private void RefreshGrid(bool Supress = false)
+        {
+            Control[] controls = OpenForm.Controls.Find(TabControl.SelectedIndex.Equals(0) ? "myLst" : "allLst", true);
+            if (controls.Count().Equals(0))
+                return;
+
+            ListBox box = (ListBox)controls[0];
+            List<string> services = new List<string>();
+            if (box.Name.Equals("allLst"))
+            {
+                for (int i = 0; i < box.Items.Count; i++)
+                {
+                    if (ServicesToDisable.Contains(box.Items[i].ToString()))
+                        services.Add(box.Items[i].ToString());
+                }
+                if (services.Any() && !Supress)
+                    MessageBox.Show($"The Following services will be limited:{Environment.NewLine}{string.Join(Environment.NewLine, services)}");
+            }
+
+            for (int i = 0; i < box.Items.Count; i++)
+            {
+                if (ServicesToDisable.Contains(box.Items[i].ToString()))
+                    box.Items.Remove(box.Items[i]);
+            }
+        }
     }
 }
---
+++ CommunityPlugin/Standard Plugins/TestPlugin.cs
@@ -0,0 +1,19 @@
+﻿using CommunityPlugin.Objects;
+using CommunityPlugin.Objects.Args;
+using CommunityPlugin.Objects.Interface;
+using EllieMae.Encompass.Automation;
+
+namespace CommunityPlugin.Standard_Plugins
+{
+    public class TestPlugin: Plugin, INativeFormLoaded
+    {
+        public override void NativeFormLoaded(object sender, FormOpenedArgs e)
+        {
+            if (e.OpenForm.Name.Equals("FormSelectioinDialog") && e.OpenForm.Text.Equals("Select Documents"))
+            {
+                System.Windows.Forms.Button b = e.OpenForm.Controls.Find("btnSend", true)[0] as System.Windows.Forms.Button;
+                b.Enabled = EncompassApplication.CurrentLoan.Fields["CX.HYBRID.OPTOUT"].FormattedValue.Equals("X");
+            }
+        }
+    }
+}
